99cc0a7f5d9a44d3b644b74fc24641a2edfffaea
matthewleffler1 <44796997+matthewleffler1@users.noreply.github.com>
2023-08-12 02:38:43 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Store drawing data temporarily (in-memory) - replace with a database for a production app
+drawings = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('draw')
+def draw(data):
+    drawings.append(data)
+    emit('drawing', data, broadcast=True)
+
+@app.route('/get_drawings')
+def get_drawings():
+    return {'drawings': drawings}
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
